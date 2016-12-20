73a53eaa8b059034746be7d6d83b7301239d300a
Adam Kocić <adam.kocic@automatkredytowy.pl>
2016-12-20 15:55:38 +0100

    zmiana logo i koloru strony

--- resources/views/layouts/main/includes/header.blade.php
+++ resources/views/layouts/main/includes/header.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="navbar-header">
                 <a class="navbar-brand" href="{{url('/')}}">
-                    <img id="logo" src="{{asset('images/np/logo.png')}}" alt="logo" />
+                    <img id="logo" src="{{asset('images/pa/logo-j.png')}}" alt="logo" />
                 </a>
             </div>
             <ul class="nav navbar-nav">
