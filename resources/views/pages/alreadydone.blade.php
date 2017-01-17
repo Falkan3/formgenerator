3a1d4c65aec34392dc0d4bf9edf9182ff074abaa
Adam Kocić <adam.kocic@automatkredytowy.pl>
2017-01-17 19:53:51 +0100

    dziekujemy za wypelnienie

--- resources/views/pages/alreadydone.blade.php
+++ resources/views/pages/alreadydone.blade.php
@@ -9,8 +9,8 @@
             <div class="row text-center">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <section>
-                        <h1><span class="text-bold text-center highlight big colored">Dziękujemy</span></h1>
-                        <h2><span class="text-center">Dziękujemy za wypełnienie ankiety. Niebawem na podany adres mailowy wyślemy do Ciebie raport.</span></h2>
+                        <h1><span class="text-bold text-center highlight big colored">Dziękujemy za wypełnienie ankiety.</span></h1>
+                        <h2><span class="text-center">Niebawem na podany adres mailowy wyślemy do Ciebie raport.</span></h2>
                         <img src="{{asset('images/pa/vivus.png')}}" class="center-block vivus-logo" alt="logo_vivus"/>
                     </section>
                 </div>
