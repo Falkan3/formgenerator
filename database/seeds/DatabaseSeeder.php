de4964c24149eff776509073838eb9e9bc57a348
Adam Kocić <adam.kocic@automatkredytowy.pl>
2017-01-18 13:11:02 +0100

    zmiany nawiasow

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -49,7 +49,7 @@ class DatabaseSeeder extends Seeder
                 3 => 'Powyżej 5'
             ]), 'rule' => 'required']);
         Question::create(['survey_id' => 1, 'step' => 2, 'number' => 3, 'name' => 'pyt3', 'type' => 'checkbox', 'text' =>
-            'Jakiej kategorii działania w Internecie prowadzisz? <span class="text-block light">(Zaznacz maksymalnie 3)</span>',
+            'Jakiej kategorii działania w Internecie prowadzisz? <span class="text-block light small">(Zaznacz maksymalnie 3)</span>',
             'values' => json_encode([
                 1 => 'blog',
                 2 => 'serwis informacyjny',
@@ -71,7 +71,7 @@ class DatabaseSeeder extends Seeder
         //Page 3
 
         Question::create(['survey_id' => 1, 'step' => 3, 'number' => 5, 'name' => 'pyt5', 'type' => 'checkbox', 'text' =>
-            'Z jakiej formy generowania przychodu z bloga/strony/serwisu korzystasz regularnie? <span class="light">(Zaznacz 3 najważniejsze)</span>', 'values' => json_encode([
+            'Z jakiej formy generowania przychodu z bloga/strony/serwisu korzystasz regularnie? <span class="light small">(Zaznacz 3 najważniejsze)</span>', 'values' => json_encode([
             1 => 'sieci afiliacyjne',
             2 => 'programy partnerskie',
             3 => 'współpraca z agencjami/ sieciami reklamowymi',
@@ -97,7 +97,7 @@ class DatabaseSeeder extends Seeder
             'rule' => 'required']);
 
         Question::create(['survey_id' => 1, 'step' => 3, 'number' => 7, 'name' => 'pyt7', 'type' => 'checkbox', 'text' =>
-            'Jakiego rodzaju kampanie wybierasz najczęściej? <span class="text-block light">(Zaznacz maksymalnie 3)</span>', 'values' => json_encode([
+            'Jakiego rodzaju kampanie wybierasz najczęściej? <span class="text-block light small">(Zaznacz maksymalnie 3)</span>', 'values' => json_encode([
             1 => 'produkty bankowe – kredyty gotówkowe',
             2 => 'produkty bankowe – inne',
             3 => 'produkty pozabankowe - pożyczki',
@@ -131,7 +131,7 @@ class DatabaseSeeder extends Seeder
             'rule' => 'required']);
 
         Question::create(['survey_id' => 1, 'step' => 3, 'number' => 10, 'name' => 'pyt10', 'type' => 'radio', 'text' =>
-            'Jaki parametr po otrzymaniu walidacji, jest dla Ciebie najważniejszy, decydujący o ewentualnej kontynuacji współpracy?', 'values' => json_encode([
+            'Jaki parametr po otrzymaniu walidacji, jest dla Ciebie najważniejszy, decydujący <span class="text-block">o ewentualnej</span> kontynuacji współpracy?', 'values' => json_encode([
             1 => '% akceptowalności',
             2 => 'wartość faktury/rozliczenia',
             3 => 'eCPL/realna wartość przychodu na leadzie',
@@ -166,9 +166,9 @@ class DatabaseSeeder extends Seeder
                 'rule' => 'required|min:5|max:5']);
 
         Question::create(['survey_id' => 1, 'step' => 4, 'number' => 13, 'name' => 'pyt13', 'type' => 'multiselect', 'text' =>
-            'Co jest najważniejszym argumentem przy wyborze tych kampanii reklamowych, 
-            (kryterium, po którym wybierasz kampanię, przy poszczególnych kryteriach wpisz wartości od 1 do 8,
-             gdzie <span class="text-nowrap">1 – najważniejsze</span>, <span class="text-nowrap">8 - kryterium</span> najmniej ważne)?',
+            'Co jest najważniejszym argumentem przy wyborze tych kampanii reklamowych?
+            <span class="light small text-block">(kryterium, po którym wybierasz kampanię, przy poszczególnych kryteriach wpisz wartości od 1 do 8,
+             gdzie <span class="text-nowrap">1 – najważniejsze</span>, <span class="text-nowrap">8 - kryterium</span> najmniej ważne)</span>',
             'values' => json_encode([
                 1 => ['question' => 'terminowe wypłaty rozliczeń', 'answers' => [1, 2, 3, 4, 5, 6, 7, 8]],
                 2 => ['question' => 'rozliczenia już po miesiącu od startu kampanii', 'answers' => [1, 2, 3, 4, 5, 6, 7, 8]],
@@ -206,7 +206,7 @@ class DatabaseSeeder extends Seeder
             'rule' => 'required']);
 
         Question::create(['survey_id' => 1, 'step' => 4, 'number' => 17, 'name' => 'pyt17', 'type' => 'checkbox', 'text' =>
-            'Co skłoniłoby Cię do promowania lub zwiększenia działań na rzecz kampanii VIVUS? <span class="text-block light">(Zaznacz maksymalnie 3)</span>', 'values' => json_encode([
+            'Co skłoniłoby Cię do promowania lub zwiększenia działań na rzecz kampanii VIVUS? <span class="text-block light small">(Zaznacz maksymalnie 3)</span>', 'values' => json_encode([
             1 => 'terminowe wypłaty rozliczeń',
             2 => 'rozliczenia już po miesiącu od startu kampanii',
             3 => 'różnorodność materiałów reklamowych/graficznych',
